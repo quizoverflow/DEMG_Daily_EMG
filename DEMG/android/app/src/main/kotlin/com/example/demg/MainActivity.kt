c1024a765cc8507b5f30df893269b52cf3757eb6
quizoverflow <jinuinssu@soongsil.ac.kr>
2024-01-28 15:03:30 +0900

    flutter project 생성

---
+++ DEMG/android/app/src/main/kotlin/com/example/demg/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demg
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ DEMG/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:demg/screen/screen_home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    )
+  );
+}
+
+
